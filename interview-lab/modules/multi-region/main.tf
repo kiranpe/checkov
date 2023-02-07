97836745d21412180c59cdf082057958e47acdba
kiranpe <36789502+kiranpe@users.noreply.github.com>
2023-02-07 21:31:48 +0530

    Delete interview-lab directory

--- interview-lab/modules/multi-region/main.tf
+++
@@ -1,7 +0,0 @@
-data "google_compute_network" "network" {
-  name = var.network
-}
-
-data "google_compute_subnetwork" "subnet" {
-  name = var.subnet
-}
--- interview-lab/modules/multi-region/service_account.tf
+++
@@ -1,4 +0,0 @@
-resource "google_service_account" "sa" {
-  account_id   = "nginx-app-sa-id"
-  display_name = "Nginx Service Account"
-}
